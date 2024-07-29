ee4b1c7f0b218caca4cc998c095638bc79ff0c6c
Bruno Bachmann <bfbachmann@gmail.com>
2024-07-28 17:51:16 -0700

    Update deployment config/scripts

--- deployment/update.sh
+++ deployment/update.sh
@@ -1,39 +1,30 @@
 #!/bin/bash
 
-set -euv -o pipefail
+set -e
 
-root=/home/ubuntu
-binary_path=$root/playground-artifacts/ui
+SOURCE_PATH="/home/bfbachmann/code/blang-playground"
+DEST_PATH="/shared/blang-playground"
 
-# Get new docker images
-$root/rust-playground/compiler/fetch.sh
+# Pull latest code from main
+echo -e "\n\n===> Pulling from main...\n"
+cd $SOURCE_PATH
+git pull origin main
 
-# Clean old docker images
-docker system prune -f || true
+# Build ui binary, update its capabilities, and copy it to the right place
+cd ui
+cargo build --release
+sudo setcap 'cap_net_bind_service=+ep' target/release/ui
 
-# Get the binary's hash so we know if it has changed
-previous_binary_hash=""
-if [[ -f "${binary_path}" ]]; then
-    previous_binary_hash=$(md5sum "${binary_path}")
-fi
+# Build frontend
+echo -e "\n\n===> Building frontend...\n"
+cd frontend
+pnpm build:production
 
-# Get new artifacts
-aws s3 sync --region=us-east-2 s3://playground-artifacts-i32 $root/playground-artifacts
-# These artifacts don't change names and might stay the same size
-# https://github.com/aws/aws-cli/issues/1074
-aws s3 sync \
-    --region=us-east-2 \
-    --exclude='*' \
-    --include=ui \
-    --include=build/index.html \
-    --include=build/index.html.gz \
-    --include=build/robots.txt \
-    --exact-timestamps \
-    s3://playground-artifacts-i32 $root/playground-artifacts
-chmod +x "${binary_path}"
+# Copy assets to shared path
+cp -r $SOURCE_PATH $DEST_PATH
 
-# Restart to get new server binary
-if [[ -z "${previous_binary_hash}" ]] || ! md5sum -c <(echo "${previous_binary_hash}") --status; then
-    sudo service playground stop || true
-    sudo service playground start
-fi
+# Restart service
+echo -e "\n\n===> Restarting service...\n"
+sudo systemctl restart blang-playground
+
+echo -e "\n\n===> DONE\n"
