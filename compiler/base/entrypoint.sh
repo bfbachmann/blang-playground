3b46407c198982013bd4021f4f82e0abd994bc43
Bruno Bachmann <bfbachmann@gmail.com>
2025-01-12 16:59:07 -0800

    Update Docker entrypoint

--- compiler/base/entrypoint.sh
+++ compiler/base/entrypoint.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
 set -eu
-
-if [[ -z "${PLAYGROUND_ORCHESTRATOR:-}" ]]; then
-    timeout=${PLAYGROUND_TIMEOUT:-10}
-
-    # Don't use `exec` here. The shell is what prints out the useful
-    # "Killed" message
-    timeout --signal=KILL ${timeout} "$@"
-else
-    exec "$@"
-fi
+exec "$@"
